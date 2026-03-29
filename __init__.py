a707e76129b946814ed9a8d12f3effacf06e4f31
james <judilsteve>
2026-03-29 11:06:24 +0800

    Expose functions in package

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+UVR Headless Runner - Audio source separation library
+
+Expose main runner functions for programmatic use.
+"""
+
+from demucs_headless_runner import run_demucs_headless
+from vr_headless_runner import run_vr_headless
+from mdx_headless_runner import run_mdx_headless
+
+__version__ = "1.1.0"
+
+__all__ = [
+    "run_demucs_headless",
+    "run_vr_headless", 
+    "run_mdx_headless",
+]
